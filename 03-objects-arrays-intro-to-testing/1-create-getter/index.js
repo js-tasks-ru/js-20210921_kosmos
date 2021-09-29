af660b4d13449f511a821f7bf00539c8e11bd15c
Konstantin Skok <skok.konstantin@gmail.com>
2021-09-29 14:52:29 +0300

    add solution for 3-module-1-task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  path = path.split(".");
 
+  return function (obj) {
+    let result = obj;
+
+    for (const prop of path) {
+      result = result[prop];
+
+      if (result === undefined || result === null) {
+        break;
+      }
+    }
+
+    return result;
+  };
 }
